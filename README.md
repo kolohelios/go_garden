f76e55d097dd67fadc64e9171d6112bf663300ae
Jon Edwards <jkedwards@me.com>
2021-11-21 21:34:07 -0800

    complete endpoints

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # go_garden
+
+Following this tutorial:
+https://www.eternaldev.com/blog/building-basic-crud-operations-in-go-with-fiber/
--- database/database.go
+++ database/database.go
@@ -21,3 +21,28 @@ func InitDatabase() error {
 
 	return nil
 }
+
+func CreateBookmark(name string, url string) (Bookmark, error) {
+	var newBookmark = Bookmark{Name: name, Url: url}
+
+	db, err := gorm.Open(sqlite.Open("bookmark.db"), &gorm.Config{})
+	if err != nil {
+		return newBookmark, err
+	}
+	db.Create(&Bookmark{Name: name, Url: url})
+
+	return newBookmark, nil
+}
+
+func GetAllBookmarks() ([]Bookmark, error) {
+	var bookmarks []Bookmark
+
+	db, err := gorm.Open(sqlite.Open("bookmark.db"), &gorm.Config{})
+	if err != nil {
+		return bookmarks, err
+	}
+
+	db.Find(&bookmarks)
+
+	return bookmarks, nil
+}
